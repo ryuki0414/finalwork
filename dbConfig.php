0384eef06c1f30f59fa3a9bee24645b027f6d4cf
ryuki0414 <ryuki0414@gmail.com>
2023-10-15 19:32:00 +0900

    first commit

---
+++ dbConfig.php
@@ -0,0 +1,11 @@
+<?php
+
+$dbName = 'mysql:host=localhost;dbname=imagePosting;charset=utf8';
+$userName = 'root';
+
+try {
+    $db = new PDO($dbName, $userName);
+    var_dump('success');
+} catch (\Throwable $th) {
+    exit();
+}
